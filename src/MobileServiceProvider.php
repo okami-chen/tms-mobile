1503cd22439021f7846916197dfafb41a3609f2b
陈德华 <chendehua@enmonster.org>
2018-07-18 14:33:15 +0800

    提交

---
+++ src/MobileServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OkamiChen\TmsMobile;
+
+use Illuminate\Support\ServiceProvider;
+
+class MobileServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
